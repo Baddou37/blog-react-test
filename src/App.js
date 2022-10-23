139db220add051e73b9800547b60f86f21ec9473
Julien Thibault <baddou@baddou-macbook.home>
2022-10-23 14:55:02 +0200

    vercel deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Containers/Home/Home';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import AddArticle from './Containers/addArticle/addArticle';
 import Contact from './Containers/Contact/Contact';
 import Article from './Containers/Article/Article';
@@ -8,15 +8,13 @@ import Article from './Containers/Article/Article';
 function App() {
   return (
     <>
-      <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
-          <Route path="/" exact end element={<Home />} />
-          <Route path="/write" exact element={<AddArticle />} />
-          <Route path="/contact" exact element={<Contact />} />
-          <Route path="/articles/:slug" exact element={<Article />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/write" element={<AddArticle />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/articles/:slug" element={<Article />} />
           </Routes>
-      </Router>
     </>
   );
 }
